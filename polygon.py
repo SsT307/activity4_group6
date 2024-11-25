4b037c7bb6418a83c4acea518eef80c7527465a6
SsT307 <shaik.alhajri22@gmail.com>
2024-11-25 04:45:56 +0400

    0

--- polygon.py
+++ polygon.py
@@ -1,22 +1,35 @@
 class Polygon:
-    __slots__ = ['__name','__sides']
+    __slots__ = ['__name','__sides'] # Makes sure that the Polygon class ONLY has these two members
+    ''' Initilize with 2 private members, name and sides '''
     def __init__(self, name, sides):
         self.__name = name
         self.__sides = sides
     
+    ''' Getting and setting the name of the polygon '''
     def get_name(self):
         return self.__name
     def set_name(self, new_value):
         self.__name = new_value
-    
+    ''' Getting and setting the number of sides of the polygon '''
     def get_sides(self):
         return self.__sides
     def set_sides(self, new_value):
         self.__sides = new_value
-        
+    
+    ''' Checking the inequality of the polygon '''
+    def __eq__(self, other):
+        if type(self) == type(other):
+            return self.__name == other.get_name() and self.__sides == other.get_sides()
+        else:
+            return False
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    ''' Creating a string representation of a polygon object '''
     def __str__(self):
         return self.__name + " with sides: " + str(self.__sides)
 
+    ''' Calculating the sum of all sides of the polygon '''
     def calculate_circumference(self):
         return sum(self.__sides)
     
--- test_polygon.py
+++ test_polygon.py
@@ -1,4 +1,12 @@
 from polygon import Polygon
+
+'''
+Test for negative
+Zeroes
+test setters
+
+'''
+
 ''' 
 *           *           *           *           *           *
 Activity 4: In this assignment we are combining our knowledge from the previous lesson of test
@@ -8,58 +16,63 @@ of an object, constructors to check if two objects are the same or not, and, fin
 to caculate the circumference/perimeter, which just adds all the sides together.
 *           *           *           *           *           *
 '''
-""" * * * * * * * * * STEP 1 * * * * * * * * * """
-""" In this step we are asked to test the getter and setter methods. We checked that the function of 
+# --------------------------------------------------------------------------------------- #
+''' *           *           *     STEP 1      *           *           * '''
+'''
+In this step we are asked to test the getter and setter methods. We checked that the function of 
 get_name() fetchesthe name of the polygon and that the setter function is able to mutate the objects 
 state, and whatever the output is will be checked with the assert function. This will ensure that 
 the given output is exactly as expected.
 We also would like to check that the polygon is, well, a polygon! We need to first check that the 
 polygon actually has sides and that it is not equal to zero, then we need to check if the sides are 
-postive numbers """
+postive numbers 
+'''
 
 def test_polygon_initialization():
-    # Test that the polygon has sides and that it is not equal to zero
-    polygon1 = Polygon('Nonagon', [])
-    assert len(polygon1.get_sides()) != 0
+    ''' Test that the polygon has sides and that it is not equal to zero '''
+    polygon1 = Polygon('Nonagon', [5,7,8])
+    assert sum(polygon1.get_sides()) != 0 # Will return True, because the total of the variables in the list does not equal to zero
     # Test that the sides are positive numbers
     polygon2 = Polygon('Shape', [1, 2, 3, 4, 5])
-    assert all(polygon2.get_sides() > 0 for polygon2.get_sides() in polygon2.sides)
+    assert all(polygon2.get_sides() > 0 for polygon2.get_sides() in polygon2.sides) # Will return True, because the sum of the
 
 def test_get_sides():
+    ''' Test that the get_sides() method returns the correct list of sides '''
     polygon = Polygon("Triangle", [3, 3, 3])  
     assert polygon.get_name() == "Triangle" 
     assert polygon.get_sides() == [3, 3, 3] 
 
 def test_set_sides():
+    ''' Test that the set_sides() method sets the sides correctly '''
     polygon = Polygon("Square", [4, 4, 4, 4])
-    polygon.set_sides([5, 5, 5, 5])  
+    polygon.set_sides([5, 5, 5, 5])  # Changes the list of sides ([4, 4, 4, 4] --> [5, 5, 5, 5])
     assert polygon.get_sides() == [5, 5, 5, 5]
 
-""" * * * * * * * * * STEP 2 * * * * * * * * * """
-def __eq__(self, other):
-        if type(self) == type(other):
-            return self.__name == other.get_name() and self.__sides == other.get_sides()
-        else:
-            return False
+# --------------------------------------------------------------------------------------- #
 
-def __ne__(self, other):
-        return not self.__eq__(other)
-
+''' *           *           *     STEP 2      *           *           * '''
 def test_polygon_equality():
+    ''' Test that the __eq__ method work correctly '''
     polygon1 = Polygon("Triangle", [3, 3, 3])
     polygon2 = Polygon("Triangle", [3, 3, 3])
-    assert polygon1.__eq__(polygon2) #will return True bcs they are equal
+    assert polygon1.__eq__(polygon2) # Will return True because they ARE equal
 
 def test_polygon_inequality():
+    ''' Test that the __ne__ method work correctly '''
     polygon3 = Polygon("Triangle", [3, 3, 3])
     polygon4 = Polygon("Square", [3, 3, 3, 3])
-    assert polygon3.__ne__(polygon4) #will return True bcs they are not equal
+    assert polygon3.__ne__(polygon4) # Will return True because they are NOT equal
 
-""" * * * * * * * * * STEP 3 * * * * * * * * * """
+
+# --------------------------------------------------------------------------------------- #
+
+''' *           *           *     STEP 3      *           *           * '''
 def test_polygon_str():
+    ''' Test that the __str__ method returns a string representation of the polygon '''
     polygon = Polygon("Triangle", [3, 3, 3])  
     assert str(polygon) == "Triangle with sides: [3, 3, 3]"
 
-def test_calc_circumference():
+def test_calculate_circumference():
+    ''' Test that the calculate_circumference method returns the sum of all sides of the polygon '''
     s1 = Polygon("Square", [4, 4, 4, 4])
-    assert s1.calculate_circumference() == 16
+    assert s1.calculate_circumference() == 16 # 4 + 4 + 4 + 4 = 16
